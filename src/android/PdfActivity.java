faf303b835e5d2b6effceb928376e615b8f04efc
Manuel Mouta <moutamanuel26@gmail.com>
2017-01-11 11:47:18 +0000

    Android fix - import added

--- src/android/PdfActivity.java
+++ src/android/PdfActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.github.barteksc.pdfviewer.PDFView;
